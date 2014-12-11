cc4b35d78631df10867fa4e5da1b2f210477584b
Eyana <eyana.mallari@obf.ateneo.edu>
2014-12-11 20:34:30 +0800

    switch case - index

--- application/controllers/admin/hardware_assets.php
+++ application/controllers/admin/hardware_assets.php
@@ -51,7 +51,23 @@ class Hardware_assets extends CI_Controller
 		if($this->input->post('report-types'))
 		{
 			$report_type = $this->extract->post();
-			var_dump($report_type); die();
+			//var_dump($report_type["report-type"]); die();
+
+			switch ($report_type["report-type"]) {
+			    case 'asset-replacement':
+			     
+			    	break; 
+			    case 'asset-recentlyadded':
+			    
+
+			    	break;
+			    case 'asset-status':
+
+			    	break;
+			    case 'asset-salvagevalue':
+			    	break;
+			    	 
+			}
 
 		}
 
@@ -298,6 +314,47 @@ class Hardware_assets extends CI_Controller
 		$this->audit_entry_model->create($audit_entry, $field_list);		
 
 	}
+
+
+	function export_information_excel($DB_TBLName, $sql, $filename)
+	{
+		$result = mysql_query($sql) or die (mysql_error());
+		    
+		$file_ending = "xls";
+
+		header("Content-Type: application/xls");    
+		header("Content-Disposition: attachment; filename=$filename.xls");  
+		header("Pragma: no-cache"); 
+		header("Expires: 0");
+
+		$sep = "\t"; //tabbed character
+		
+		for ($i = 0; $i < mysql_num_fields($result); $i++) 
+		{
+		echo mysql_field_name($result,$i) . "\t";
+		}
+		print("\n");    
+	//end of printing column names  
+	//start while loop to get data
+	    while($row = mysql_fetch_row($result))
+	    {
+	        $schema_insert = "";
+	        for($j=0; $j<mysql_num_fields($result);$j++)
+	        {
+	            if(!isset($row[$j]))
+	                $schema_insert .= "NULL".$sep;
+	            elseif ($row[$j] != "")
+	                $schema_insert .= "$row[$j]".$sep;
+	            else
+	                $schema_insert .= "".$sep;
+	        }
+	        $schema_insert = str_replace($sep."$", "", $schema_insert);
+	        $schema_insert = preg_replace("/\r\n|\n\r|\n|\r/", " ", $schema_insert);
+	        $schema_insert .= "\t";
+	        print(trim($schema_insert));
+	        print "\n";
+	    } 
+	}	
 
 
 
