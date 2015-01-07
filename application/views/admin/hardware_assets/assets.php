b13bbbee4bff0670bcd2b142088c8e1ef645b691
Eyana <eyana.mallari@obf.ateneo.edu>
2015-01-07 15:13:12 +0800

    working on ajax passing on filter functions

---
+++ application/views/admin/hardware_assets/assets.php
@@ -0,0 +1,53 @@
+
+	<table class="table-list table-striped table-bordered">
+		<thead>
+			<tr>
+				<th></th>
+				<th>Asset Barcode</th>
+				<th>Asset Type</th>
+				<th>Model</th>
+				<th>Status</th>
+				<th>Tech Refresher</th>
+
+				
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		foreach($hardware_assets->result() as $hardware_asset)
+		{
+			?>
+			<tr>
+				<td class="center"><input type="checkbox" name="har_barcodes[]" value="<?php echo $hardware_asset->har_barcode; ?>" /></td>
+				<td><a href="<?php echo site_url('admin/hardware_assets/view/' . $hardware_asset->har_barcode); ?>"><?php echo $hardware_asset->har_barcode; ?></a></td>
+				<td><?php echo $hardware_asset->har_asset_type; ?></td>				
+				<td><?php echo $hardware_asset->har_model; ?></td>
+				<td>
+
+				<?php if($hardware_asset->har_status=='active'):?>
+
+					<span class="label label-success"><?php echo $hardware_asset->har_status; ?></span>
+
+				<?php elseif ($hardware_asset->har_status=='repair'): ?>
+
+					<span class="label label-warning"><?php echo $hardware_asset->har_status; ?></span>
+
+				<?php else: ?>
+
+					<span class="label label-default"><?php echo $hardware_asset->har_status; ?></span>
+
+					
+
+				<?php endif; ?>
+
+				</td>
+				<td><?php echo $hardware_asset->har_tech_refresher; ?></td>
+				
+			</tr>
+			<?php
+		}
+		?>
+		</tbody>
+	</table>
+	<?php echo $hardware_assets_pagination; ?>
+	
